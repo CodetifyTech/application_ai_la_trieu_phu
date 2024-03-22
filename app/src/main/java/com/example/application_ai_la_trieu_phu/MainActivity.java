e83166d1bac6f3cf43a58f51839d6e33b9e2535d
nguyenthanhtrung2k2dz <125016933+nguyenthanhtrung2k2dz@users.noreply.github.com>
2024-03-22 16:27:59 +0700

    Update

--- app/src/main/java/com/example/application_ai_la_trieu_phu/MainActivity.java
+++ app/src/main/java/com/example/application_ai_la_trieu_phu/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.application_ai_la_trieu_phu;
 
+import static com.example.application_ai_la_trieu_phu.R.id.btn_hd;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
+    Button btn_start, btn_hd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.mh_start);
+        anhXa();
+        xuly();
+    }
+
+    private void anhXa() {
+        btn_start = findViewById(R.id.btn_start);
+        btn_hd = findViewById(R.id.btn_hd);
+    }
+
+    private void xuly() {
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Bạn Đã Sẵn Sàng Đến Với Chương Trình Của Chúng Tôi", Toast.LENGTH_SHORT).show();
+
+            startGame();
+            }
         });
+
+    }
+
+    public void startGame(){
+    Intent intent = new Intent(this, mh_null.class);
+    startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/application_ai_la_trieu_phu/mh_null.java
@@ -0,0 +1,24 @@
+package com.example.application_ai_la_trieu_phu;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class mh_null extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.mh_null);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}

ca233e157b94cebd8319aba9163e8a2e941a6b37
TrDuyKhanh <mailkhanh02@gmail.com>
2024-03-24 15:36:04 +0700

    part Interface v1

--- app/src/main/java/com/example/application_ai_la_trieu_phu/mh_player.java
+++ app/src/main/java/com/example/application_ai_la_trieu_phu/mh_player.java
@@ -1,9 +1,6 @@
 package com.example.application_ai_la_trieu_phu;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,29 +8,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Button btn;
+public class mh_player extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        btn  = (Button)findViewById(R.id.btn);
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.layout_play);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, mh_player.class);
-                startActivity(intent);
-            }
-        });
     }
+
+
+
 }
